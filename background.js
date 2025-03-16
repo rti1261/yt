e25c50eab285fb872ed7dfe4345d636e35748f1f
rti1261 <rti1261@outlook.com>
2025-03-16 04:21:53 -0700

    Update background.js

--- background.js
+++ background.js
@@ -1,3 +1,17 @@
 chrome.runtime.onInstalled.addListener(() => {
-    console.log("YouTube Auto Enhancer Installed!");
+  console.log("YouTube Auto Enhancer Installed!");
+  console.log("Mouse Tracker Spoofer Extension Installed");
+
+  // Save the tracking data (hardcoded example, you can modify it to load dynamically if needed)
+  chrome.storage.local.set({
+    trackingData: [
+      { time: '2025-03-16T11:02:42.495Z', event: 'mousemove', element: 'YTD-MESSAGE-RENDERER', x: 286, y: 655 },
+      { time: '2025-03-16T11:02:42.501Z', event: 'mousemove', element: 'YTD-MESSAGE-RENDERER', x: 288, y: 653 },
+      { time: '2025-03-16T11:02:42.510Z', event: 'mousemove', element: 'YTD-MESSAGE-RENDERER', x: 289, y: 653 },
+      // Add more events here...
+    ]
+  });
+
+  // Optionally, log that the data has been stored
+  console.log("Tracking data saved to local storage.");
 });
